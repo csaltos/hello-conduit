464fe768eaa7e39c8cd73314863d4bfd1cc13a19
Carlos Saltos <csaltos@gmail.com>
2021-05-03 14:45:30 +0300

    Fixing Either body cases

--- app/Main.hs
+++ app/Main.hs
@@ -3,11 +3,13 @@
 module Main where
 
 import Data.Aeson ( Value )
-import Network.HTTP.Simple ( httpJSONEither, Response, JSONException )
+import Network.HTTP.Simple ( httpJSONEither, Response, JSONException, getResponseBody )
 
 main :: IO ()
 main = do
     response <- httpJSONEither "http://httpbin.org/get" :: IO (Response (Either JSONException Value))
-    case response of
-        r ->
-            print r
+    case getResponseBody response of
+        Right body ->
+            print body
+        Left error ->
+            print error
